003ef04139c7f452faa8838064d2d24542cf85bf
Emily Ryan <emilyryan@Emilys-MacBook-Pro.local>
2021-01-30 22:31:55 -0600

    set up migrations

--- data/migrations/20210130215433_co-make.js
+++ data/migrations/20210130215433_co-make.js
@@ -1,8 +1,30 @@
 
 exports.up = function(knex) {
+    return knex.schema
+      .createTable('users', table => {
+        table.increments('id')
+        table.string('username', 255).notNullable().unique().index()
+        table.string('email', 255).notNullable().unique().index()
+        table.string('password', 255).notNullable()
+      })
+      .createTable('issues', table => {
+        table.increments('id')
+        table.string('title', 255).notNullable()
+        table.string('description').notNullable()
+        table.timestamp('date_created').defaultTo(knex.fn.now())
+        table.string('resolved_status').defaultTo('unresolved')
+        table.integer('user_id')
+          .unsigned()
+          .references('id')
+          .inTable('users')
+          .onDelete('CASCADE')
+          .onUpdate('CASCADE')
+      })
+      
+  };
   
-};
-
-exports.down = function(knex) {
-  
-};
+  exports.down = function(knex) {
+    return knex.schema
+      .dropTableIfExists('issues')
+      .dropTableIfExists('users');
+  };
