e86e0cec3def354b3025217b2bced0b03b0ee3ee
Benaiah Varner <benaiahvarner@Benaiahs-MBP.home>
2021-02-01 12:33:12 -0500

    all endpoints except the hard one

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,13 +1,32 @@
 const router = require('express').Router()
 const Users = require('./users-model')
 
-router.get('/', (req, res) => {
-    Users.get()
-    .then((user) => {
-        res.status(200).json(user)
+router.put('/:id', (req, res) => {
+    const changes = req.body
+    const id = req.params.id
+    console.log(id, changes)
+    Users.update(id, changes)
+    .then(user => {
+        if (user) {
+          res.status(200).json(user);
+        } else {
+          res.status(404).json({ message: 'The user could not be found' });
+        }
     })
-    .catch(err => res.status(500).json({ message: err }))
+    .catch(err => {res.status(500).json(err)
+        console.log(err)
+    })
 })
 
+router.delete('/:id', (req, res) => {
+    Users.remove(req.params.id)
+    .then(user => {
+        console.log(user)
+        res.status(200).json({ message: `account deleted` })
+    })
+    .catch(err => {res.status(500).json(err)
+        console.log(err)
+    })
+})
 
 module.exports = router
